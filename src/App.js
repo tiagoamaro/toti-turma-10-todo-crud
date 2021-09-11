6d5355882d77291faf8ed6c77fbe9e65f20531c6
Tiago Amaro <tiagopadrela@gmail.com>
2021-09-11 20:03:11 -0300

    Using React hooks and routing for the app

--- src/App.js
+++ src/App.js
@@ -1,42 +1,61 @@
 import './App.css';
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Lista from "./Lista"
 import CriarTarefa from './CriarTarefa';
+import EditarTarefa from './EditarTarefa';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-// Lista de tarefas
-// -> Listar tarefa por tarefa
-// -> tarefa
 
-export default class App extends React.Component {
-  constructor() {
-    super()
+export default function App() {
+  const [tasks, setTasks] = useState([])
 
-    this.state = {
-      tasks: [],
-      version: 0,
-    }
-
-    this.fetchTasks = this.fetchTasks.bind(this);
-  }
-
-  fetchTasks() {
+  useEffect(() => {
     fetch("http://localhost:3001/tasks")
       .then(response => response.json())
-      .then(tasks => this.setState({ tasks: tasks, version: this.state.version + 1 }))
-  }
+      .then(tasks => setTasks(tasks))
+  }, [])
 
-  componentDidMount() {
-    this.fetchTasks()
-  }
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Listar</Link>
+        <br />
+        <Link to="/create">Criar</Link>
+        <br />
+        <Link to="/sobre">Sobre rotas</Link>
+      </nav>
 
-  render() {
-    return (
-      <div className="App">
-        <CriarTarefa createTaskCallback={this.fetchTasks} />
+      <Switch>
+        <Route path="/create">
+          <CriarTarefa />
+        </Route>
+        <Route path="/edit/:id">
+          <EditarTarefa />
+        </Route>
+        <Route path="/sobre">
+          Essa branch mostra como nós podemos usar rotas, hooks e componentes funcionais para escrever uma versão mais
+          moderna e prática de componentes React, sem precisar passar funções de callback e ter uma maior preocupação com
+          gerenciamento de estados!
 
-        <Lista tasks={this.state.tasks} fetchTasksCallback={this.fetchTasks} />
-      </div>
-    );
-  }
+          Boa sorte galera! =D
+
+          Vocês podem ver mais sobre rotas e hooks nas aulas gravadas, e na documentação do React:
+
+          <ul>
+            <li>Rotas: <a href="https://reactrouter.com/web">https://reactrouter.com/web</a></li>
+            <li>Hooks: <a href="https://reactjs.org/docs/hooks-intro.html">https://reactjs.org/docs/hooks-intro.html</a></li>
+          </ul>
+        </Route>
+        <Route path="/">
+          <Lista tasks={tasks} />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
